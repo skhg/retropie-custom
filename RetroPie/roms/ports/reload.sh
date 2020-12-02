5bc69a3f36154f912e5b3ce0e1c160f406e1a3a4
Jack <git@jackhiggins.ie>
2020-12-03 00:28:35 +0100

    add a reload script from ui

---
+++ RetroPie/roms/ports/reload.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd ~/retropie-custom || exit
+git pull
+./setup.sh
--- RetroPie/roms/ports/youtube.sh
+++ RetroPie/roms/ports/youtube.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 source ~/retropie-custom/chromium/preconfigure.sh youtube
