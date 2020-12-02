354896b6ea914eef028e562874001abac5d36fcc
Jack <git@jackhiggins.ie>
2020-12-02 23:23:25 +0100

    rom launcher scripts

---
+++ RetroPie/roms/ports/Listen to Wikipedia.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+source ~/retropie-custom/chromium/preconfigure.sh listen_wiki
--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
 
+set -exo pipefail
+
+echo "Map the QJoyPad configurations..."
+
+rm -rf ~/.qjoypad3
+ln -s ~/retropie-custom/qjoypad3 ~/.qjoypad3
+
+echo "Map the chromium-browser configurations..."
+
+
+echo "Map the chromium launchers..."
