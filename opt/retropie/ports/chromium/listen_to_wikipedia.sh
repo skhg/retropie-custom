243e6001f0591e57455f3862c9daadfe688d6d2a
Jack <git@jackhiggins.ie>
2020-12-03 00:44:53 +0100

    unclutter for listen wiki

--- opt/retropie/ports/chromium/listen_to_wikipedia.sh
+++ opt/retropie/ports/chromium/listen_to_wikipedia.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 xset -dpms s off s noblank
 matchbox-window-manager -use_titlebar no &
+unclutter &
 qjoypad "iNNext" &
 /usr/bin/chromium-browser --use-gl=egl --kiosk --start-fullscreen --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-extensions --disable-component-update --app=http://listen.hatnote.com/
 
