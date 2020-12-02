07b96367936be6aac0e8eaaf72fbdd7e6ab6d64f
skhg <git@jackhiggins.ie>
2020-12-03 00:21:46 +0100

    map the desktop launcher scripts

--- setup.sh
+++ setup.sh
@@ -12,10 +12,18 @@ echo "Map the chromium-browser configurations..."
 rm /opt/retropie/configs/ports/chromium/emulators.cfg
 ln -s ~/retropie-custom/opt/retropie/configs/ports/chromium/emulators.cfg /opt/retropie/configs/ports/chromium/emulators.cfg
 
-echo "Map the chromium launchers..."
+echo "Map the RetroPie chromium launchers..."
 
 for filename in ~/retropie-custom/RetroPie/roms/ports/*.sh; do
 	base=$(basename "$filename")
 	echo "Mapping $base..."
 	ln -sf "$filename" ~/RetroPie/roms/ports/"$base"
+done
+
+echo "Map the chromium desktop environment launchers..."
+
+for filename in ~/retropie-custom/opt/retropie/ports/chromium/*.sh; do
+	base=$(basename "$filename")
+	echo "Mapping $base..."
+	sudo ln -sf "$filename" /opt/retropie/ports/chromium/"$base"
 done
