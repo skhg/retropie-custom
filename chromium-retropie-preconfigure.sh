275d7ba60444d5bf617def0712957b9e25673863
skhg <git@jackhiggins.ie>
2020-12-02 22:34:55 +0100

    add preconfigure script

---
+++ chromium-retropie-preconfigure.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -exo pipefail
+
+echo "Starting chromium-browser with configuration: \"$1\"..."
+
+echo "Configuring chromium-browser dependencies..."
+
+sudo apt-get -y install xserver-xorg-legacy qjoypad unclutter 
+
+echo "Configuring chromium launcher..."
+
+sed -i '$ d' /opt/retropie/configs/ports/chromium/emulators.cfg
+echo "default = \"$1\"" >> /opt/retropie/configs/ports/chromium/emulators.cfg
+"/opt/retropie/supplementary/runcommand/runcommand.sh" 0 _PORT_ "chromium" ""
