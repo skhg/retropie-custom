781df17c7961f17d0738efd5dddd1d6b78b92ccb
Jack <git@jackhiggins.ie>
2020-12-03 01:04:04 +0100

    safer restart

--- RetroPie/roms/ports/reload.sh
+++ RetroPie/roms/ports/reload.sh
@@ -3,4 +3,6 @@
 cd ~/retropie-custom || exit
 git pull
 ./setup.sh
+pid=$(pidof /opt/retropie/supplementary/emulationstation/emulationstation)
+kill "$pid"
 sudo systemctl restart autologin@tty1.service
