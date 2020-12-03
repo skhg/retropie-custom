a1a1ebde5e883a21f433cf2d8386e003166c76f7
Jack <git@jackhiggins.ie>
2020-12-03 01:06:37 +0100

    remove the auto restart that doesnt work

--- RetroPie/roms/ports/reload.sh
+++ RetroPie/roms/ports/reload.sh
@@ -3,6 +3,3 @@
 cd ~/retropie-custom || exit
 git pull
 ./setup.sh
-pid=$(pidof /opt/retropie/supplementary/emulationstation/emulationstation)
-kill "$pid"
-sudo systemctl restart autologin@tty1.service
