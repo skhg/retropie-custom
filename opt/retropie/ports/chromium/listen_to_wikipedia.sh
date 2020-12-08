21fa06babfb30e7cdd57b7b290626218752c4508
skhg <git@jackhiggins.ie>
2020-12-08 23:48:03 +0100

    use my custom version with fullscreen

--- opt/retropie/ports/chromium/listen_to_wikipedia.sh
+++ opt/retropie/ports/chromium/listen_to_wikipedia.sh
@@ -3,4 +3,4 @@ xset -dpms s off s noblank
 matchbox-window-manager -use_titlebar no &
 unclutter &
 qjoypad "iNNext" &
-/usr/bin/chromium-browser --use-gl=egl --autoplay-policy=no-user-gesture-required --kiosk --start-fullscreen --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-extensions --disable-component-update --app=http://listen.hatnote.com/
+/usr/bin/chromium-browser --use-gl=egl --autoplay-policy=no-user-gesture-required --kiosk --start-fullscreen --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-extensions --disable-component-update --app=http://jackhiggins.ie/listen-to-wikipedia?fullscreen
